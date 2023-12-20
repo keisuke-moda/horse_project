ac30f381da356120aec00d9f6abfcfb056673243
keisuke-moda <pyunta1128388@gmail.com>
2023-12-21 01:33:41 +0900

    topページを多少作り始めた

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
+<script setup>
+</script>
+
 <template>
-    <div>ハローワールドです。</div>
+    <div class="m-4">
+        <a>2023年の収支</a>
+        <div class="flex justify-center">
+            <div>収支</div>
+            <div>$</div>
+        </div>
+        <div class="flex">
+            <div>購入</div>
+            <div>$</div>
+        </div>
+        <div class="flex">
+            <div>払い戻し</div>
+            <div>$</div>
+        </div>
+        <div class="flex">
+            <div>回収率</div>
+            <div>%</div>
+        </div>
+    </div>
 </template>
+
+<style scoped></style>
