9bc610644ee577eea7a684e122213c38ef356706
keisuke-moda <pyunta1128388@gmail.com>
2025-02-27 23:10:02 +0900

    画面を分割するようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 const router = useRouter()
-
-const changeRegisterPage = () => {
-    router.push('/register-race')
-}
-
-const changeDataPage = () => {
-    router.push('/data')
-}
 </script>
 
 <template>
-    <div>タイトルページ</div>
-    <button @click="changeRegisterPage()">登録画面へ</button>
-    <button @click="changeDataPage()">データ分析画面</button>
+    <div class="title">タイトルページ</div>
+    <button @click="router.push('/register-race')">登録画面へ</button>
+    <button @click="router.push('/data')">データ分析画面</button>
+    <button @click="router.push('/result')">過去の結果一覧</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ pages/result/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
+<template>
+    <div>
+        テスト。過去の馬券結果一覧を確認したい。
+    </div>
+    <button @click="router.go(-1)">戻る</button>
+</template>
